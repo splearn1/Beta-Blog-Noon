60e5a4a3399c2c2cb2b0160ccc3300c7ba8e0d9f
Nolan Hovis <nolan.hovisPersonal@gmail.com>
2024-03-04 12:59:18 -0600

    CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:4200"
+    origins "https://master--betablogsnoon.netlify.app/"
 
     resource "*",
       headers: :any,
